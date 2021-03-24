2126d8e17d8053244e149d7fdcec4c79fefab25b
Vsumargo <vincent.sumargo@gmail.com>
2021-03-24 13:23:44 +0800

    add engine node version to package.json

--- server.js
+++ server.js
@@ -8,18 +8,16 @@ const router = require(path.join(__dirname,'routes','routeIndex.js'));
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/fitnessTracker", {
+  useNewUrlParser: true,
+});
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(router)
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/fitnessTracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
